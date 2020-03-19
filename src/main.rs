2cd8746c28a77739f0182d7230626732d240d906
Evin Owen <github@evinowen.net>
2020-03-19 09:40:56 -0700

    Add basic routes to service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate iron;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+fn health(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "health")))
+}
+
+fn manifest(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "manifest")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = Router::new();
+
+    router.get("/", health, "health");
+    router.post("/", manifest, "manifest");
+
+    Iron::new(router).http("0.0.0.0:8080").unwrap();
 }
